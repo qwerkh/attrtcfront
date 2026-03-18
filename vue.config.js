d21541567731dc060555aac088a9f08df0eef84b
qwerkh <narongsao98@gmail.com>
2026-03-18 15:49:32 +0700

    d

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    host: "0.0.0.0",
-    allowedHosts: "all",
-    client: {
-      webSocketURL: {
-        protocol: "wss",
-        hostname: "corban-serumal-dodie.ngrok-free.dev",
-        port: 0,
-        pathname: "/ws"
-      }
+    transpileDependencies: true,
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableLegacy: true,
+            runtimeOnly: false,
+            compositionOnly: true,
+            fullInstall: true
+        },
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        }
     }
-  },
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableLegacy: true,
-      runtimeOnly: false,
-      compositionOnly: true,
-      fullInstall: true
-    },
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
 })
