5e7bf741ec1dd579db8ca2c50f49cfb7ffdea3e2
qwerkh <narongsao98@gmail.com>
2026-04-27 12:19:42 +0700

    d

--- vue.config.js
+++ vue.config.js
@@ -28,6 +28,13 @@ module.exports = defineConfig({
             display: 'standalone',
             background_color: '#ffffff'
         },
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        }
 
     }
 })
